bfa47f3886a46a72336a5f2cdd9cc3b562a97898
ninidani27 <ninidani2712@gmail.com>
2025-09-10 12:42:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const track = document.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+
+let currentSlide = 0;
+let autoPlayInterval;
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateCarousel();
+}
+
+function startAutoPlay() {
+  autoPlayInterval = setInterval(nextSlide, 5000);
+}
+
+function stopAutoPlay() {
+  clearInterval(autoPlayInterval);
+  startAutoPlay();
+}
+
+nextButton.addEventListener("click", () => {
+  nextSlide();
+  stopAutoPlay();
+});
+prevButton.addEventListener("click", () => {
+  prevSlide();
+  stopAutoPlay();
+});
+
+startAutoPlay();
